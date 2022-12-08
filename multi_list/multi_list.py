4d01941b99415f8057c90f4ac958f4aa6ac47b6a
kvirikroma <kvirikroma@gmail.com>
2022-12-08 22:26:45 +0200

    ability to search and add items

--- multi_list/multi_list.py
+++ multi_list/multi_list.py
@@ -1,4 +1,4 @@
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Union
 from collections import deque
 
 
@@ -17,7 +17,9 @@ class MultiListPath:
     def __init__(self, path_sequence: Iterable[int] | str, separator: str = "/"):
         self.separator = separator
         if isinstance(path_sequence, str):
-            self._sequence = tuple(int(i) for i in path_sequence.strip().strip(self.separator).split(self.separator))
+            self._sequence = tuple(
+                int(i) for i in path_sequence.strip().strip(self.separator).split(self.separator) if i
+            )
         else:
             self._sequence = tuple(path_sequence)
 
@@ -33,6 +35,13 @@ class MultiListPath:
     def __iter__(self):
         return iter(self._sequence)
 
+    def __getitem__(self, item) -> Union[int, "MultiListPath"]:
+        result = self._sequence.__getitem__(item)
+        return MultiListPath(result) if isinstance(item, slice) else result
+
+    def __bool__(self):
+        return bool(self._sequence)
+
     def startswith(self, other: "MultiListPath"):
         if len(other) > len(self):
             return False
@@ -45,7 +54,6 @@ class MultiListPath:
 class MultiList:
     def __init__(self):
         self.root: MultiListNode | None = None
-        self._iteration_item: MultiListNode | None = None
         self._items_count = 0
 
     def iterate(self, include_all_levels: bool = True):
@@ -83,14 +91,63 @@ class MultiList:
             print("]", end='')
         print()
 
+    def _find_node(self, path: MultiListPath) -> MultiListNode | None:
+        if not path:
+            return None
+        node = self.root
+        path_part_index = 0
+        for index in path:
+            assert index >= 0
+            for i in range(index):
+                if not node:
+                    return None
+                node = node.right
+            if not node:
+                return None
+            path_part_index += 1
+            if path_part_index < len(path):
+                node = node.child.root if node.child else None
+                if not node:
+                    return None
+        return node
+
     def exists(self, path: MultiListPath) -> bool:
-        pass
+        return self._find_node(path) is not None
 
-    def find(self, path: MultiListPath) -> MultiListNode:
-        pass
+    def find(self, path: MultiListPath):
+        node = self._find_node(path)
+        if not node:
+            raise LookupError("Path does not exist")
+        return node.value
 
-    def add(self, node: MultiListNode, path: MultiListPath):
-        pass
+    def append(self, value, path: MultiListPath):
+        assert path[-1] >= 0
+        parent_node = MultiListNode(None, None, self) if len(path) == 1 else self._find_node(path[:-1])
+        if parent_node is None:
+            raise LookupError("Path does not exist")
+        if parent_node.child:
+            if parent_node.child.root:
+                if parent_node.child._items_count < path[-1]:
+                    raise LookupError("Path does not exist")
+                node = parent_node.child.root
+                if path[-1] == 0:
+                    parent_node.child.root = MultiListNode(value, right=node)
+                else:
+                    for _ in range(path[-1] - 1):
+                        node = node.right
+                    node.right = MultiListNode(value, right=node.right)
+                parent_node.child._items_count += 1
+            else:
+                if path[-1] != 0:
+                    raise LookupError("Path does not exist")
+                parent_node.child.root = MultiListNode(value)
+                parent_node.child._items_count = 1
+        else:
+            if path[-1] != 0:
+                raise LookupError("Path does not exist")
+            parent_node.child = MultiList()
+            parent_node.child.root = MultiListNode(value)
+            parent_node.child._items_count = 1
 
     def delete(self, path: MultiListPath):
         pass
