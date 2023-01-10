3aa209194828550cfca14b917d05715cec6cc3b7
kvirikroma <kvirikroma@gmail.com>
2023-01-10 16:17:38 +0200

    started skip_list

---
+++ skip_list/skip_list.py
@@ -0,0 +1,12 @@
+from typing import Iterable
+
+
+class SkipListNode:
+    def __init__(self, value, level: int, right_neighbors: Iterable["SkipListNode"] = None):
+        self.level = level
+        if right_neighbors:
+            
+
+
+class SkipList:
+    pass
