eb8e41ba41842f9444b34103a2a657d9a5f11661
kvirikroma <kvirikroma@gmail.com>
2023-01-12 23:34:09 +0200

    updated skip_list

--- skip_list/skip_list.py
+++ skip_list/skip_list.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Iterable
+from typing import List, Callable, Iterable, Tuple
 from math import log2
 from random import randint
 
@@ -6,24 +6,23 @@ from random import randint
 class SkipListNode:
     def __init__(self, value, levels: int):
         self.value = value
-        self.levels = levels
         self.right: List[SkipListNode | None] = [None for _ in range(levels)]
 
     def __repr__(self):
-        return f"SkipListNode(value={self.value}, levels={self.levels}"
+        return f"SkipListNode(value={self.value}, levels={len(self.right)}"
 
 
 class SkipList:
     def __init__(self, max_level: Callable[[int], int] | int | None = None):
         self.count = 0
         self.root: List[SkipListNode] = []
-        self.max_level = max_level if max_level else lambda count: (log2(count) if self.count >= 2 else 1)
+        self.max_level = max_level if max_level else lambda count: (log2(count) // 1 if self.count >= 2 else 1)
 
-    def _generate_level_randomly(self):
+    def _generate_level_randomly(self) -> int:
         max_level = self.max_level if isinstance(self.max_level, int) else self.max_level(self.count)
         min_level = 1
-        result = max_level - (log2(randint(2 ** min_level, (2 ** (max_level + 1)) - 1)) // 1) + min_level
-        return min(result, len(self.root))
+        result = max_level - int(log2(randint(2 ** min_level, (2 ** (max_level + 1)) - 1)) // 1) + min_level
+        return min(result, len(self.root) + 1) - 1
 
     def _find_node(self, value, return_previous_if_absent: bool = False) -> SkipListNode | None:
         if not self.root:
@@ -44,27 +43,42 @@ class SkipList:
         if return_previous_if_absent:
             return node
 
-    def _find_previous_node(self, value) -> SkipListNode | None:
+    def _find_previous_node(self, value) -> Tuple[SkipListNode, List[SkipListNode | None]] | None:
         if not self.root:
             return None
         node = self.root[-1]
+        left_connections = [None for _ in self.root]
         for level in reversed(range(len(self.root))):
             while True:
                 if node.value >= value:
                     return None
                 if node.value < value:
                     if not node.right[level]:
+                        left_connections[level] = node
                         break
                     if node.right[level].value >= value:
+                        left_connections[level] = node
                         break
                     node = node.right[level]
-        return node
+        return node, left_connections
 
     def from_iterable(self, source: Iterable, tree_like: bool = False):
         pass
 
     def append(self, value):
-        pass
+        prev_node_search = self._find_previous_node(value)
+        new_node = SkipListNode(value, self._generate_level_randomly())
+        if prev_node_search:
+            prev_node, connections = prev_node_search
+            prev_node_right = prev_node.right
+        else:
+            prev_node_right = connections = self.root
+        if prev_node_right[0] and prev_node_right[0].value == value:
+            raise ValueError("This value already exists in the list")
+        for level in range(len(new_node.right)):
+            new_node.right[level] = connections[level]
+            if connections[level]:
+                connections[level].right[level] = new_node
 
     def delete(self, value):
         pass
@@ -81,14 +95,14 @@ class SkipList:
         destination_node.right[0] = None
         true_max_level = 1
         while source_node := source_node.right[0]:
-            destination_node.right[0] = SkipListNode(source_node.value, source_node.levels)
+            destination_node.right[0] = SkipListNode(source_node.value, len(source_node.right))
             destination_node = destination_node.right[0]
-            true_max_level = max(true_max_level, source_node.levels)
+            true_max_level = max(true_max_level, len(source_node.right))
         result.root = [None for _ in range(len(self.root))]
         last_node_on_level: List[SkipListNode | None] = [None for _ in range(true_max_level)]
         node = start_node
         while node := node.right[0]:
-            for level in range(node.levels):
+            for level in range(len(node.right)):
                 if last_node_on_level[level]:
                     last_node_on_level[level].right[level] = node
                 if not result.root[level]:
